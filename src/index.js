5db6b65a14943413e64908a3949a987b8e4b949a
TP1997 <thanhphamlike@gmail.com>
2021-07-13 15:51:43 -0400

    Added contact-tab

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import navbar from "./navbar.js";
 import about from "./about-tab.js";
 import menu from "./menu-tab.js";
+import contact from "./contact-tab.js";
 function main(){
   const CONTENT = document.getElementById('content');
-  CONTENT.appendChild(menu());
+  CONTENT.appendChild(navbar());
+  CONTENT.appendChild(about());
 }
 
 main();
