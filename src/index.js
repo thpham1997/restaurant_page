d8569d761d7a502d0a309c048053f086e480f244
TP1997 <thanhphamlike@gmail.com>
2021-07-13 23:14:08 -0400

    Added css file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import navbar from "./navbar.js";
 import about from "./about-tab.js";
 import menu from "./menu-tab.js";
 import contact from "./contact-tab.js";
+import '../dist/css/main.css'
 function main(){
   const CONTENT = document.getElementById('content');
   CONTENT.appendChild(navbar());
