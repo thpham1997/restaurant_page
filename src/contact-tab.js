89cd15c983e0eff64f13a1e81305ca872226e6fc
TP1997 <thanhphamlike@gmail.com>
2021-07-16 23:01:45 -0400

    Adjusted the struture by adding two more parent divs

--- src/contact-tab.js
+++ src/contact-tab.js
@@ -1,6 +1,8 @@
 import myMap from './img/map-contact.png'
 function contact(){
   let contactDiv = document.createElement('div');
+  let textDiv = document.createElement('div');
+  let embedMapDiv = document.createElement('div');
   let name = document.createElement('p');
   let address = document.createElement('p');
   let phone = document.createElement('p');
@@ -8,25 +10,29 @@ function contact(){
   let map = document.createElement('div');
 
   name.innerHTML = "TP's Ramen and sushi";
-  address.innerHTML = 'This is an address';
-  phone.innerHTML='(443)-111-2222';
-  email.innerHTML='tprs@gmail.com';
+  address.innerHTML = 'Addres: this is an address';
+  phone.innerHTML='Phone: (443)-111-2222';
+  email.innerHTML='Email: tprs@gmail.com';
   map = new Image();
   map.src = myMap;
 
-
-  contactDiv.appendChild(name);
-  contactDiv.appendChild(address);
-  contactDiv.appendChild(phone);
-  contactDiv.appendChild(email);
-  contactDiv.appendChild(map);
+  
+  textDiv.appendChild(name);
+  textDiv.appendChild(address);
+  textDiv.appendChild(phone);
+  textDiv.appendChild(email);
+  embedMapDiv.appendChild(map);
+  contactDiv.appendChild(textDiv);
+  contactDiv.appendChild(embedMapDiv);
   // class adding
   contactDiv.classList.add('contact');
-  name.classList.add('contact__name');
-  address.classList.add('contact__address');
-  phone.classList.add('contact__phone');
-  email.classList.add('contact__email');
-  map.classList.add('contact__map');
+  textDiv.classList.add('contact__information');
+  embedMapDiv.classList.add('contact__embed');
+  name.classList.add('contact__information__name');
+  address.classList.add('contact__information__address');
+  phone.classList.add('contact__information__phone');
+  email.classList.add('contact__information__email');
+  map.classList.add('contact__embed__map');
   
   // end of class adding
   return contactDiv;
